f74b9a553cdcf995c568bf5eea462732b97575d2
Roy-Tasiame <roy.tasiame@ashesi.edu.gh>
2019-11-20 20:10:09 +0000

    Update index.php

--- MCSWEBAPP2/viewAndController/index.php
+++ MCSWEBAPP2/viewAndController/index.php
@@ -44,7 +44,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title>Home - mcsSite2</title>
+        <title>Home | Cozy Hill</title>
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
         <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
