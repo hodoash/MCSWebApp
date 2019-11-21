d28fe79b2cb546bc2c7130de1fe31aed3473f63f
eleanor-gb <55196935+eleanor-gb@users.noreply.github.com>
2019-11-21 01:07:47 +0000

    Add files via upload

---
+++ mcs_db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+    //declare variables for my database connection 
+	define('SERVERNAME', 'cs.ashesi.edu.gh');
+	define('USERNAME', 'eleanor_gbedawo');
+	define('PASSWORD', 'eleanor_gbedawo');
+	define('DATABASE', 'webtech_fall2019_eleanor_gbedawo');
+
+?>
